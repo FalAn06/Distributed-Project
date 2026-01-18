cc07b7dff72e0b3b7a73ee1aed115e681869b853
FalAn06 <159930269+FalAn06@users.noreply.github.com>
2026-01-18 13:19:45 -0500

    feat: :bug: fix phrase

--- User-Domain/biblio_store_service/app/services/frases_service.py
+++ User-Domain/biblio_store_service/app/services/frases_service.py
@@ -4,12 +4,12 @@ import random
 def obtener_frases():
     # Lista de frases acerca de la tienda de agua
     return [
-        "El agua es vida. ¡Descúbrela con nosotros!",
-        "La mejor tienda de productos de agua, siempre fresca.",
-        "Donde el agua es más que una necesidad, es una experiencia.",
-        "Productos de agua para toda tu familia, ¡aquí los encuentras!",
-        "Refresca tu vida con el agua de la mejor calidad.",
-        "¡Cuida tu salud con nuestra selección de aguas puras!",
-        "El agua más pura para tu hogar, en un solo lugar.",
-        "No solo vendemos agua, vendemos bienestar."
+        "Un libro es una puerta a nuevos mundos.",
+        "La biblioteca: donde el conocimiento cobra vida.",
+        "Leer es viajar sin moverse del lugar.",
+        "Miles de historias te esperan en nuestra biblioteca.",
+        "El saber está al alcance de todos, entra y descubre.",
+        "Un espacio para aprender, imaginar y crecer.",
+        "La lectura alimenta la mente y el alma.",
+        "En cada libro hay una nueva aventura por comenzar."
     ]
