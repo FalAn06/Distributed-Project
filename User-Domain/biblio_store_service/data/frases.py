e6dcf1b72ae4565470682ce764dfed9f4d41f064
FalAn06 <159930269+FalAn06@users.noreply.github.com>
2026-01-18 13:13:58 -0500

    feat: :sparkles: new micro

---
+++ User-Domain/biblio_store_service/data/frases.py
@@ -0,0 +1,12 @@
+def obtener_frases():
+    # Lista de frases acerca de la biblioteca
+    return [
+        "Un libro es una puerta a nuevos mundos.",
+        "La biblioteca: donde el conocimiento cobra vida.",
+        "Leer es viajar sin moverse del lugar.",
+        "Miles de historias te esperan en nuestra biblioteca.",
+        "El saber está al alcance de todos, entra y descubre.",
+        "Un espacio para aprender, imaginar y crecer.",
+        "La lectura alimenta la mente y el alma.",
+        "En cada libro hay una nueva aventura por comenzar."
+    ]
